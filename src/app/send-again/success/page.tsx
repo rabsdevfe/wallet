672378ab6cba2248a23f368e27c412b3e75504d7
rabsdevfe <rabs.dev.fe@gmail.com>
2025-07-24 19:16:41 -0300

    feat: Adding main projects pages

---
+++ src/app/send-again/success/page.tsx
@@ -0,0 +1,7 @@
+export default function SendAgainSuccessPage() {
+  return (
+    <div className="p-8">
+      <div className="text-2xl font-bold mb-8">Send Again Success View</div>
+    </div>
+  );
+}
---
+++ src/app/transfers/page.tsx
@@ -0,0 +1,7 @@
+export default function TransfersPage() {
+  return (
+    <div className="p-8">
+      <div className="text-2xl font-bold mb-8">Transfers View</div>
+    </div>
+  );
+}
